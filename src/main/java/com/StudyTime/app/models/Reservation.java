547670323701b7a3f64d968737fb7a37cd312963
MeDefNot <mynuha224@gmail.com>
2026-06-30 13:09:48 +0600

    Add Reservation entity with fields and relationships

--- src/main/java/com/StudyTime/app/models/Reservation.java
+++ src/main/java/com/StudyTime/app/models/Reservation.java
@@ -1,5 +1,45 @@
 package com.StudyTime.app.models;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "reservations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reservation {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Reservation_id")
+    private long id;
+
+    @Column(name = "Booking_Date", nullable = false)
+    private LocalDateTime bookingDate;
+
+    @Column(name = "Time_Slot_Starting", nullable = false)
+    private LocalDateTime timeSlotStarting;
+
+    @Column(name = "Time_Slot_Ending", nullable = false)
+    private LocalDateTime timeSlotEnding;
+
+    @Column(name = "Reservation_Status", nullable = false)
+    private String reservationStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "Student_id", nullable = false)
+    private Student student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Seat_no", nullable = false)
+    private Seat seat;
+
+    @PrePersist
+    protected void onCreate(){
+        this.bookingDate = LocalDateTime.now();
+    }
 }
