711a9eaa43ada290d4e35b8a1ab98d2c2fe7551c
MeDefNot <mynuha224@gmail.com>
2026-06-30 15:52:41 +0600

    Add Seat entity with JPA annotations

--- src/main/java/com/StudyTime/app/models/Seat.java
+++ src/main/java/com/StudyTime/app/models/Seat.java
@@ -1,5 +1,26 @@
 package com.StudyTime.app.models;
 
+//import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Seat")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Seat {
-    
+
+    @Id
+    @Column(name = "Seat_no", nullable = false)
+    private String seatNo;
+
+    @Column(name = "Availability", nullable = false)
+    private boolean availability;
+
+    @Column(name = "Priority_Type", nullable = false)
+    private String priorityType;
 }
