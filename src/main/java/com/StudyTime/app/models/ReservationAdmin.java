4c45bdd658ce3dc02228746972c4efc271ae32b1
MeDefNot <mynuha224@gmail.com>
2026-06-30 15:53:06 +0600

    Add relationships for Reservation and Administrator

--- src/main/java/com/StudyTime/app/models/ReservationAdmin.java
+++ src/main/java/com/StudyTime/app/models/ReservationAdmin.java
@@ -1,5 +1,23 @@
 package com.StudyTime.app.models;
 
+//import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Reservation_Admin")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservationAdmin {
-    
+    @ManyToOne
+    @JoinColumn(name = "Reservation_id", nullable = false)
+    private Reservation reservation;
+
+    @ManyToOne
+    @JoinColumn(name = "Admin_id", nullable = false)
+    private Administrator admin;
 }
