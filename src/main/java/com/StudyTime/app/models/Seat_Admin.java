756a67f92e070f10e76e60de81b758c8169406da
MeDefNot <mynuha224@gmail.com>
2026-07-02 22:16:12 +0600

    Update Seat_Admin.java

--- src/main/java/com/StudyTime/app/models/Seat_Admin.java
+++ src/main/java/com/StudyTime/app/models/Seat_Admin.java
@@ -1,5 +1,26 @@
 package com.StudyTime.app.models;
 
-public class Seat_Admin {
-    
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Seat_Admin")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SeatAdmin {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "Seat_Admin_Id")
+   private Long id;  
+
+   @ManyToOne
+   @JoinColumn(name = "Seat_no", nullable = false)
+   private Seat seat;
+
+   @ManyToOne
+   @JoinColumn(name = "Admin_Id", nullable = false)
+   private Administrator admin;
 }
