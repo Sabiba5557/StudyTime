e7c65f27eedd50b5aa9af070f611f46e6c4e10a8
MeDefNot <mynuha224@gmail.com>
2026-07-02 22:11:51 +0600

    Add BorrowRecord entity with JPA annotations

--- src/main/java/com/StudyTime/app/models/BorrowRecord.java
+++ src/main/java/com/StudyTime/app/models/BorrowRecord.java
@@ -1,5 +1,33 @@
 package com.StudyTime.app.models;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+ 
+@Entity
+@Table(name = "Borrow_Record")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BorrowRecord {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Borrow_Id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "Book_Id", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "Student_Id", nullable = false)
+    private Student student;
+
+    @Column(name = "Borrow_Date", nullable = false)
+    private LocalDateTime borrowDate;
+
+
 }
