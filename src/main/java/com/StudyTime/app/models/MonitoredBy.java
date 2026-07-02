7ec545130dc99be2b0f53066952c054b30440201
MeDefNot <mynuha224@gmail.com>
2026-07-02 22:13:33 +0600

    Add MonitoredBy entity with JPA annotations

--- src/main/java/com/StudyTime/app/models/MonitoredBy.java
+++ src/main/java/com/StudyTime/app/models/MonitoredBy.java
@@ -1,5 +1,26 @@
 package com.StudyTime.app.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Monitored_By")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MonitoredBy {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Monitored_By_Id")
+    private Long monitoredById;
+
+    @ManyToOne
+    @JoinColumn(name = "Complaint_Id", nullable = false)
+    private Complaint complain;
+
+    @ManyToOne
+    @JoinColumn(name = "Admin_Id", nullable = false)
+    private Administrator admin;
 }
