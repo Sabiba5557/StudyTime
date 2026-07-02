54193503edef5f6dc94370dc56aeaf5de1da37e6
MeDefNot <mynuha224@gmail.com>
2026-07-02 22:17:04 +0600

    Add StudentAdmin entity with relationships

--- src/main/java/com/StudyTime/app/models/StudentAdmin.java
+++ src/main/java/com/StudyTime/app/models/StudentAdmin.java
@@ -1,5 +1,26 @@
 package com.StudyTime.app.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Student_Admin")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentAdmin {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Student_Admin_Id")
+    private Long studentAdminId;
+
+    @ManyToOne
+    @JoinColumn(name = "Student_Id", nullable = false)
+    private Student student;    
+
+    @ManyToOne
+    @JoinColumn(name = "Admin_Id", nullable = false)
+    private Administrator admin;
 }
