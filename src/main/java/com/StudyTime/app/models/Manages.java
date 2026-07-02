3a5c90e3db5165b00851c93c66603f2b2259d3c5
MeDefNot <mynuha224@gmail.com>
2026-07-02 22:13:02 +0600

    Add Manages entity with relationships to Admin and Library

--- src/main/java/com/StudyTime/app/models/Manages.java
+++ src/main/java/com/StudyTime/app/models/Manages.java
@@ -1,5 +1,28 @@
 package com.StudyTime.app.models;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "Manages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Manages {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Manage_Id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "Admin_Id", nullable = false)
+    private Administrator admin;
+
+    @ManyToOne
+    @JoinColumn(name = "Hall_Name", nullable = false)
+    private Library library;
 }
