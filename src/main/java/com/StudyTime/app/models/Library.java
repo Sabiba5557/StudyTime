bf68d7686240cdf732ae43e03241a39be146e547
MeDefNot <mynuha224@gmail.com>
2026-07-02 22:12:30 +0600

    Add Library entity with hall and time attributes

--- src/main/java/com/StudyTime/app/models/Library.java
+++ src/main/java/com/StudyTime/app/models/Library.java
@@ -1,5 +1,25 @@
 package com.StudyTime.app.models;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Library")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Library {
-    
+    @Id
+    @Column(name = "Hall_Name", nullable = false)
+    private String hallName;
+
+    @Column(name = "Opening_Time", nullable = false)
+    private LocalDateTime openingTime;
+
+    @Column(name = "Closing_Time", nullable = false)
+    private LocalDateTime closingTime;
 }
