327eca02a45cb8e4b1a958cf6531b5bc4edea541
MeDefNot <mynuha224@gmail.com>
2026-07-02 22:12:11 +0600

    Add Complaint entity with necessary fields

--- src/main/java/com/StudyTime/app/models/Complaint.java
+++ src/main/java/com/StudyTime/app/models/Complaint.java
@@ -1,5 +1,37 @@
 package com.StudyTime.app.models;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Complaint")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Complaint {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Complaint_Id", nullable = false)
+    private Long complaintId;
+
+    @Column(name = "Complain_Category", nullable = false)
+    private String complainCategory;
+
+    @Column(name = "Description", nullable = false)
+    private String description;
+
+    @Column(name = "Submission_Date", nullable = false)
+    private LocalDateTime submissionDate;
+
+    @Column(name = "Status", nullable = false)
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "Student_Id", nullable = false)
+    private Student student;
+
 }
